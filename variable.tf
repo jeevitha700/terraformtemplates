38f0775cd69e820db05fb313c9c40208efc9efc9
jeevitha700 <133192759+jeevitha700@users.noreply.github.com>
2023-07-27 20:53:44 +0530

    done

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "ami"{
+    default= "ami-0f5ee92e2d63afc18"
+}
+
+variable "type"{
+    default="t2.micro"
+}
+variable  "REGION" {
+ default = "ap-south-1"
+}
+ variable "host" {
+ default = "13.126.217.131"
+}
